ef941d9ccb671879a8b03ad8a651d0ad83cffeba
tb9757 <tb9757@gmail.com>
2025-10-27 11:02:36 +0000

    Add widgets

--- streamlitNew.py
+++ streamlitNew.py
@@ -1,3 +1,8 @@
 import streamlit as st 
 
+#versatile display function
 st.write('Hello, Streamlit!')
+
+#widgets (input from people)
+name = st.text_input('Enter your name: ')
+age = st.number_input('Enter your age: ', min_value=1, max_value=120)
